4fb02bc0486751e71157d84da3c7cd34c0d50bba
raxen001 <raxen001@tutamail.com>
2024-11-23 17:24:02 +0530

    lol

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 - Need docker preinstalled
 - `docker compose up` on root folder to start ollama instance
 - `docker exec -it {{container-id}} bash`
-    - `ollama pull llama3.2`
+    - ~`ollama pull llama3.2`~
+    - `ollama pull qwen2.5-coder:7b`
 - `pip install -r requirements.txt`
 - `python3 src/main.py`
--- src/util.py
+++ src/util.py
@@ -1,29 +1,29 @@
 import ollama
 from ollama import Client
+
 from pprint import pprint
+
 import os
+from guidance import models, gen
 
 HOST = os.environ.get("LLAMA_HOST", "http://localhost:11434")
-# MODEL = os.environ.get("LLAMA_MODEL", "llama3.2")
-
+MODEL = os.environ.get("LLAMA_MODEL", "qwen2.5-coder:7b")
 client = Client(
   host=HOST
 )
 
 MODELNAME='raven'
 MODELFILE='''
-FROM llama3.2
-SYSTEM You are a DevOps bot created to only output the docker compose files for the relevant input according to the folder structure and files and libraries present in the project only return the valid docker compose and don't return anything else.
+FROM qwen2.5-coder:7b
+SYSTEM you should only ouput the necessary dockercompose.yml and nothing else. no verbose output
 '''
 ollama.create(model=MODELNAME, modelfile=MODELFILE)
 
 
-content = "Create me a python3 docker container to run basic python addition program"
+content = "Create me a python3 docker container to run my main.py"
 response = client.generate(
     model=MODELNAME,
     prompt=content,
 )
 
-pprint(dir(response))
-print()
-pprint(response)
+pprint(response['response'])
